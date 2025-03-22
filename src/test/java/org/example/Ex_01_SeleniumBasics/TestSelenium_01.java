fccc8145ff40b4356a78761e4cfe4ab145da07aa
HP <saurabhtyagi020@gmail.com>
2025-03-22 23:53:31 +0530

    22 March

--- src/test/java/org/example/Ex_01_SeleniumBasics/TestSelenium_01.java
+++ src/test/java/org/example/Ex_01_SeleniumBasics/TestSelenium_01.java
@@ -1,4 +1,15 @@
 package org.example.Ex_01_SeleniumBasics;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium_01 {
+
+    @Test
+    public void test_Sample()
+    {
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
 }
